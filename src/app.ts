cbb58099f8a14a153789c578fc2cfb8e62387bbd
Bolaji Ayodeji <sirbeejay1@gmail.com>
2025-04-16 06:57:21 +0100

    test: update image path

--- src/app.ts
+++ src/app.ts
@@ -107,7 +107,7 @@ async function generateMarkdown() {
   <a href="https://blog.bolajiayodeji.com/how-to-create-an-automated-profile-readme-using-nodejs-and-github-actions?utm_source=github-profile">Learn how this works.</a> <a href="https://github.com/BolajiAyodeji/BolajiAyodeji/actions/workflows/build.yml"><img src="https://github.com/BolajiAyodeji/BolajiAyodeji/actions/workflows/build.yml/badge.svg" align="right" alt="Rebuild README.md file"></a>\n
    
   <div align="center">\n
-   <a href="https://bolajiayodeji.com" target="_blank" rel="noopener noreferrer"><img src="https://bolajiayodeji.com/favicon.png" width="30" /></a>\n
+   <a href="https://bolajiayodeji.com" target="_blank" rel="noopener noreferrer"><img src="https://github.com/BolajiAyodeji/bolajiayodeji.com/blob/main/public/favicon.png?raw=true" width="30" /></a>\n
   </div>`;
 
   const result = md.render(markdownText);
